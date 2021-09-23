ad2153cb482914a87697bffda4eac0510e981f95
haha <haha@gmail.com>
2021-09-24 00:03:18 +0700

    t

--- dist/providers/fmovies.js
+++ dist/providers/fmovies.js
@@ -55,8 +55,13 @@ source.getResource = function (movieInfo, config, callback) { return __awaiter(_
                     var type = parseSearch(item).find('.fdi-type').text();
                     libs.log({ title: title, href: href, year: year, type: type }, PROVIDER, 'SEARCH ITEM');
                     if (title && href) {
-                        if (libs.string_matching_title(movieInfo, title) && movieInfo.year == year) {
-                            LINK_DETAIL = "" + DOMAIN + href;
+                        if (libs.string_matching_title(movieInfo, title)) {
+                            if (movieInfo.type == 'tv') {
+                                LINK_DETAIL = "" + DOMAIN + href;
+                            }
+                            if (movieInfo.type == 'movie' && movieInfo.year == year) {
+                                LINK_DETAIL = "" + DOMAIN + href;
+                            }
                         }
                     }
                 });
