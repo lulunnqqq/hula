cbdd8b7b27ba3233fb79f04cc86853923fbb9965
haha <haha@gmail.com>
2021-10-18 09:23:14 +0700

    b

--- dist/hosts/mcloud.js
+++ dist/hosts/mcloud.js
@@ -36,9 +36,9 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
 };
 var _this = this;
 hosts["mcloud"] = function (url, movieInfo, provider, config, callback) { return __awaiter(_this, void 0, void 0, function () {
-    var HOST, urlEmbed, dataEmbed, rank, _i, _a, embedItem, embedData, patternQuality, _b, patternQuality_1, patternItem, urlDirect, urlDirect;
-    return __generator(this, function (_c) {
-        switch (_c.label) {
+    var HOST, urlEmbed, dataEmbed, rank, _i, _a, embedItem, embedData;
+    return __generator(this, function (_b) {
+        switch (_b.label) {
             case 0:
                 HOST = 'MCLOUD';
                 urlEmbed = url.replace('/embed/', '/info/');
@@ -46,14 +46,14 @@ hosts["mcloud"] = function (url, movieInfo, provider, config, callback) { return
                         referer: url
                     }, false)];
             case 1:
-                dataEmbed = _c.sent();
+                dataEmbed = _b.sent();
                 libs.log({ urlEmbed: urlEmbed, dataEmbed: dataEmbed, subs: config.subs }, provider, 'DATA EMBED');
                 if (!dataEmbed || !dataEmbed.success || !dataEmbed.media || !dataEmbed.media.sources) {
                     return [2];
                 }
                 rank = 0;
                 _i = 0, _a = dataEmbed.media.sources;
-                _c.label = 2;
+                _b.label = 2;
             case 2:
                 if (!(_i < _a.length)) return [3, 5];
                 embedItem = _a[_i];
@@ -63,35 +63,12 @@ hosts["mcloud"] = function (url, movieInfo, provider, config, callback) { return
                 if (!(embedItem.file.indexOf('vidstream') != -1 || embedItem.file.indexOf('mcloud.to') != -1)) return [3, 4];
                 return [4, libs.request_get(embedItem.file, {})];
             case 3:
-                embedData = _c.sent();
+                embedData = _b.sent();
                 if (!embedData) {
                     return [3, 4];
                 }
-                patternQuality = embedData.match(/hls\/([0-9]+)\/[0-9]+\.m3u8/ig);
-                libs.log({ patternQuality: patternQuality, file: embedItem.file }, provider, 'PATTERN QUALITY');
-                if (!patternQuality) {
-                    libs.embed_callback(embedItem.file, provider, HOST, 'Hls', callback, ++rank, config.subs ? config.subs : []);
-                    return [3, 4];
-                }
-                for (_b = 0, patternQuality_1 = patternQuality; _b < patternQuality_1.length; _b++) {
-                    patternItem = patternQuality_1[_b];
-                    if (patternItem.indexOf('360') != -1 || patternItem.indexOf('480') != -1) {
-                        if (patternQuality.length > 2) {
-                            continue;
-                        }
-                    }
-                    if (embedItem.file.indexOf("list.m3u8#.mp4") != -1) {
-                        urlDirect = embedItem.file.replace('list.m3u8#.mp4', patternItem);
-                        libs.log({ urlDirect: urlDirect }, provider, 'URL DIRECR REPLACE');
-                        libs.embed_callback(urlDirect, provider, HOST, 'Hls', callback, ++rank, config.subs ? config.subs : []);
-                    }
-                    else if (embedItem.file.indexOf("list.m3u8") != -1) {
-                        urlDirect = embedItem.file.replace('list.m3u8', patternItem);
-                        libs.log({ urlDirect: urlDirect }, provider, 'URL DIRECR REPLACE');
-                        libs.embed_callback(urlDirect, provider, HOST, 'Hls', callback, ++rank, config.subs ? config.subs : []);
-                    }
-                }
-                _c.label = 4;
+                libs.embed_callback(embedItem.file, provider, HOST, 'Hls', callback, ++rank, config.subs ? config.subs : []);
+                _b.label = 4;
             case 4:
                 _i++;
                 return [3, 2];
