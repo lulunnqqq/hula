c57d10cfa4e014ae38230123ce332215ea2e55c4
haha <haha@gmail.com>
2023-03-01 23:53:14 +0700

    t

--- dist/providers/m4ufree.js
+++ dist/providers/m4ufree.js
@@ -36,21 +36,83 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
 };
 var _this = this;
 source.getResource = function (movieInfo, config, callback) { return __awaiter(_this, void 0, void 0, function () {
-    var PROVIDER, DOMAIN, userAgent, urlSearch, LINK_DETAIL, parseTvDetail_1, hasLinkTv_1;
+    var PROVIDER, DOMAIN, userAgent, headers, urlSearch, cookieData, laravel_session, xsrf, cookie, parseSearch_1, LINK_DETAIL_1, LINK_TV_1, parseTvDetail_1, hasLinkTv_1, e_1;
     return __generator(this, function (_a) {
         switch (_a.label) {
             case 0:
                 PROVIDER = 'M4UFREE';
                 DOMAIN = "https://ww1.m4ufree.tv";
                 userAgent = libs.request_getRandomUserAgent();
-                urlSearch = "".concat(DOMAIN, "/search/").concat(libs.url_slug_search(movieInfo, '-', true, 2), ".html");
-                libs.log({ urlSearch: urlSearch }, PROVIDER, 'URL SEARCH');
-                LINK_DETAIL = "https://ww1.m4ufree.tv/watch-yibaa-thor-love-and-thunder-2022-movie-online-free-m4ufree.html";
-                if (!(movieInfo.type == "tv")) return [3, 2];
-                return [4, libs.request_get(LINK_DETAIL, {}, true)];
+                headers = {
+                    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36 Edg/111.0.1660.14',
+                    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+                    'Sec-Fetch-Site': 'none',
+                    'Sec-Fetch-Mode': 'navigate',
+                    'Sec-Fetch-User': '?1',
+                    'Sec-Fetch-Dest': 'document',
+                    'Host': 'ww1.m4ufree.tv',
+                    'sec-ch-ua': '"Microsoft Edge";v="111", "Not(A:Brand";v="8", "Chromium";v="111"',
+                    'Upgrade-Insecure-Requests': 1,
+                    'Connection': 'keep-alive',
+                    'sec-ch-ua-mobile': '?0',
+                    'sec-ch-ua-platform': '"macOS"',
+                    'Accept-Language': 'en-US,en;q=0.9'
+                };
+                _a.label = 1;
             case 1:
+                _a.trys.push([1, 9, , 10]);
+                urlSearch = "".concat(DOMAIN, "/search/").concat(libs.url_slug_search(movieInfo, '-', true, 2), ".html");
+                return [4, libs.cookies_clearAll()];
+            case 2:
+                _a.sent();
+                return [4, libs.request_get("".concat(DOMAIN, "/"), headers)];
+            case 3:
+                _a.sent();
+                return [4, libs.cookies_get(DOMAIN)];
+            case 4:
+                cookieData = _a.sent();
+                return [4, libs.cookies_clearAll()];
+            case 5:
+                _a.sent();
+                libs.log({ cookieData: cookieData }, PROVIDER, 'COOKIE DATA');
+                laravel_session = cookieData['laravel_session']['value'];
+                xsrf = cookieData['XSRF-TOKEN']['value'];
+                cookie = "laravel_session=".concat(laravel_session, "; XSRF-TOKEN=").concat(xsrf);
+                headers['cookie'] = cookie;
+                return [4, libs.request_get(urlSearch, headers, true)];
+            case 6:
+                parseSearch_1 = _a.sent();
+                libs.log({
+                    length: parseSearch_1('.row.border .item').length
+                }, PROVIDER, 'SEARCH LENGTH');
+                LINK_DETAIL_1 = '';
+                parseSearch_1('.row.border .item').each(function (key, item) {
+                    var tipTitle = parseSearch_1(item).find('.tiptitle').text();
+                    var year = tipTitle.match(/\(([0-9]+)/i);
+                    year = year ? year[1] : 0;
+                    var title = tipTitle.replace(/\( *[0-9]+ *\)/i, '').trim();
+                    var href = parseSearch_1(item).find('.imagecover a').attr('href');
+                    libs.log({ title: title, tipTitle: tipTitle, year: year, href: href }, PROVIDER, 'SEARCH INFO');
+                    if (libs.string_matching_title(movieInfo, title)) {
+                        if (movieInfo.type == 'tv') {
+                            LINK_DETAIL_1 = "".concat(DOMAIN, "/").concat(href);
+                        }
+                        else if (movieInfo.type == 'movie' && year == movieInfo.year) {
+                            LINK_DETAIL_1 = "".concat(DOMAIN, "/").concat(href);
+                        }
+                    }
+                });
+                libs.log({ LINK_DETAIL: LINK_DETAIL_1 }, PROVIDER, 'LINK_DETAIL');
+                if (!LINK_DETAIL_1) {
+                    return [2];
+                }
+                LINK_TV_1 = '';
+                if (!(movieInfo.type == "tv")) return [3, 8];
+                return [4, libs.request_get(LINK_DETAIL_1, headers, true)];
+            case 7:
                 parseTvDetail_1 = _a.sent();
                 hasLinkTv_1 = false;
+                libs.log({ length: parseTvDetail_1('.episode').length }, PROVIDER, "TV LENGTH");
                 parseTvDetail_1('.episode').each(function (key, item) {
                     var id = parseTvDetail_1(item).attr('idepisode');
                     var epiLInk = parseTvDetail_1(item).attr('epilink');
@@ -59,35 +121,42 @@ source.getResource = function (movieInfo, config, callback) { return __awaiter(_
                     var rowname = parseTvDetail_1(item).attr('rowname');
                     libs.log({ id: id, epiLInk: epiLInk, season: season, episode: episode, rowname: rowname }, PROVIDER, 'TVSHOW MATCHING');
                     if (season == movieInfo.season && episode == movieInfo.episode) {
-                        LINK_DETAIL = "".concat(DOMAIN, "/tvshow/").concat(rowname, "-season-").concat(season, "-episode-").concat(episode, "-").concat(epiLInk, ".html");
-                        libs.log(LINK_DETAIL, PROVIDER, 'LINK TV DETAIL');
+                        LINK_TV_1 = "".concat(DOMAIN, "/tvshow/").concat(rowname, "-season-").concat(season, "-episode-").concat(episode, "-").concat(epiLInk, ".html");
+                        libs.log(LINK_TV_1, PROVIDER, 'LINK TV DETAIL');
                         hasLinkTv_1 = true;
                     }
                 });
+                libs.log({ hasLinkTv: hasLinkTv_1, LINK_TV: LINK_TV_1 }, PROVIDER, "LINK TV");
                 if (!hasLinkTv_1) {
                     return [2];
                 }
-                _a.label = 2;
-            case 2:
+                LINK_DETAIL_1 = LINK_TV_1;
+                _a.label = 8;
+            case 8:
+                libs.log({ LINK_DETAIL: LINK_DETAIL_1 }, PROVIDER, 'LINK DETAIL END');
                 callback({
                     callback: {
                         provider: PROVIDER,
                         host: PROVIDER,
-                        url: LINK_DETAIL,
-                        headers: {
-                            'user-agent': userAgent
-                        },
+                        url: LINK_DETAIL_1,
+                        headers: headers,
                         callback: callback,
-                        beforeLoadScript: "var open = XMLHttpRequest.prototype.open;\n            XMLHttpRequest.prototype.open = function() {\n                this.addEventListener(\"load\", function() {\n                    var message = {\"status\" : this.status, \"responseURL\" : this.responseURL, \"responseText\": this.responseText, \"response\": this.response}\n                    \n                    window.ReactNativeWebView.postMessage(JSON.stringify(message));\n                });\n                open.apply(this, arguments);\n            };",
+                        beforeLoadScript: "var open = XMLHttpRequest.prototype.open;\n                XMLHttpRequest.prototype.open = function() {\n                    this.addEventListener(\"load\", function() {\n                        var message = {\"status\" : this.status, \"responseURL\" : this.responseURL, \"responseText\": this.responseText, \"response\": this.response}\n                        \n                        window.ReactNativeWebView.postMessage(JSON.stringify(message));\n                    });\n                    open.apply(this, arguments);\n                };",
                         metadata: {
                             PROVIDER: PROVIDER,
                             DOMAIN: DOMAIN,
                             movieInfo: movieInfo,
                             userAgent: userAgent,
+                            url_webview: LINK_DETAIL_1,
                         }
                     }
                 });
-                return [2, true];
+                return [3, 10];
+            case 9:
+                e_1 = _a.sent();
+                libs.log({ e: e_1 }, PROVIDER, 'ERROR M4UFREE');
+                return [3, 10];
+            case 10: return [2];
         }
     });
 }); };
