a31a40b532ba68fe74780bee3c071509da035f6e
haha <haha@gmail.com>
2023-12-27 00:18:52 +0700

    t

--- dist/providers/idflix.js
+++ dist/providers/idflix.js
@@ -36,7 +36,7 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
 };
 var _this = this;
 source.getResource = function (movieInfo, config, callback) { return __awaiter(_this, void 0, void 0, function () {
-    var CryptoJSAesJson, PROVIDER, DOMAIN, urlSearch, parseSeach, id, headers, body, urlAjaxEmbed, embedData, decode, test, e_1;
+    var CryptoJSAesJson, PROVIDER, DOMAIN, urlSearch, parseSeach_1, id, headers, numes_2, _loop_1, _i, numes_1, nume, state_1, e_1;
     return __generator(this, function (_a) {
         switch (_a.label) {
             case 0:
@@ -75,56 +75,131 @@ source.getResource = function (movieInfo, config, callback) { return __awaiter(_
                     }
                 };
                 PROVIDER = 'DIdFlix';
-                DOMAIN = "https://tv.idlixplus.net/";
+                DOMAIN = "https://netmovies.to";
                 _a.label = 1;
             case 1:
-                _a.trys.push([1, 5, , 6]);
+                _a.trys.push([1, 7, , 8]);
                 urlSearch = "";
                 if (movieInfo.type == 'movie') {
                     urlSearch = "".concat(DOMAIN, "/movie/").concat(libs.url_slug_search(movieInfo), "-").concat(movieInfo.year);
                 }
                 else {
-                    urlSearch = "".concat(DOMAIN, "/episode/").concat(libs.url_slug_search(movieInfo), "-season-").concat(movieInfo.season, "-episode-").concat(movieInfo.episode);
+                    urlSearch = "".concat(DOMAIN, "/episodes/").concat(libs.url_slug_search(movieInfo), "-").concat(movieInfo.season, "x").concat(movieInfo.episode);
                 }
                 libs.log({ urlSearch: urlSearch }, PROVIDER, "URL SEARCH");
                 return [4, libs.request_get(urlSearch, {}, true)];
             case 2:
-                parseSeach = _a.sent();
-                id = parseSeach("#dooplay-ajax-counter").attr("data-postid");
+                parseSeach_1 = _a.sent();
+                id = parseSeach_1("#player-option-1").attr("data-post");
                 libs.log({ urlSearch: urlSearch, id: id }, PROVIDER, "URL SEARCH");
                 headers = {
                     "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
                     "Referer": urlSearch
                 };
-                body = qs.stringify({
-                    action: "doo_player_ajax",
-                    post: id,
-                    nume: 1,
-                    type: movieInfo.type == 'movie' ? 'movie' : 'tv'
+                numes_2 = [];
+                parseSeach_1('.dooplay_player_option').each(function (key, item) {
+                    var n = parseSeach_1(item).attr("data-nume");
+                    if (n) {
+                        numes_2.push(n);
+                    }
                 });
-                urlAjaxEmbed = "".concat(DOMAIN, "/wp-admin/admin-ajax.php");
-                return [4, libs.request_post(urlAjaxEmbed, headers, body)];
+                _loop_1 = function (nume) {
+                    var body, urlAjaxEmbed, embedData, parseEmbed, scriptEval, unpack, fileHls, parseHls, qualityMatch, directQuality, _b, qualityMatch_1, item, quality;
+                    return __generator(this, function (_c) {
+                        switch (_c.label) {
+                            case 0:
+                                body = qs.stringify({
+                                    action: "doo_player_ajax",
+                                    post: id,
+                                    nume: nume,
+                                    type: movieInfo.type == 'movie' ? 'movie' : 'tv'
+                                });
+                                urlAjaxEmbed = "".concat(DOMAIN, "/wp-admin/admin-ajax.php");
+                                return [4, libs.request_post(urlAjaxEmbed, headers, body)];
+                            case 1:
+                                embedData = _c.sent();
+                                libs.log({ embedData: embedData, body: body, headers: headers, urlAjaxEmbed: urlAjaxEmbed }, PROVIDER, "EMBED DATA");
+                                if (!embedData.embed_url) {
+                                    return [2, { value: void 0 }];
+                                }
+                                if (embedData.embed_url.indexOf("play.netembed.xyz") == -1) {
+                                    libs.embed_redirect(embedData.embed_url, '', movieInfo, PROVIDER, callback, "", [], {
+                                        link_detail: urlSearch
+                                    });
+                                    return [2, "continue"];
+                                }
+                                return [4, libs.request_get(embedData.embed_url, {}, true)];
+                            case 2:
+                                parseEmbed = _c.sent();
+                                scriptEval = '';
+                                parseEmbed('script').each(function (key, item) {
+                                    if (parseEmbed(item).text().indexOf('eval(') != -1) {
+                                        scriptEval = parseEmbed(item).text();
+                                    }
+                                });
+                                if (!scriptEval) {
+                                    return [2, { value: void 0 }];
+                                }
+                                unpack = libs.string_unpack(scriptEval);
+                                libs.log({ unpack: unpack }, PROVIDER, 'UNPACK');
+                                fileHls = unpack.match(/file *\' *\: *\'([^\']+)/i);
+                                fileHls = fileHls ? fileHls[1] : '';
+                                if (!fileHls) {
+                                    return [2, { value: void 0 }];
+                                }
+                                libs.log({ fileHls: fileHls }, PROVIDER, 'FILE HLS');
+                                return [4, libs.request_get(fileHls, {})];
+                            case 3:
+                                parseHls = _c.sent();
+                                qualityMatch = parseHls.match(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/ig);
+                                libs.log({ qualityMatch: qualityMatch }, PROVIDER, 'QUALITY MATCH');
+                                directQuality = [];
+                                for (_b = 0, qualityMatch_1 = qualityMatch; _b < qualityMatch_1.length; _b++) {
+                                    item = qualityMatch_1[_b];
+                                    if (item.indexOf("quality=") == -1) {
+                                        continue;
+                                    }
+                                    quality = item.match(/quality=([0-9]+)/i);
+                                    libs.log({ quality: quality }, PROVIDER, 'quality');
+                                    quality = quality ? Number(quality[1]) : 720;
+                                    if (isNaN(quality) && quality < 360) {
+                                        quality = 720;
+                                    }
+                                    directQuality.push({
+                                        file: item,
+                                        quality: quality,
+                                    });
+                                }
+                                if (directQuality.length == 0) {
+                                    return [2, { value: void 0 }];
+                                }
+                                directQuality = _.sortBy(directQuality, ['quality'], ['desc']);
+                                libs.log({ directQuality: directQuality }, PROVIDER, 'DIRECT QUALITY');
+                                libs.embed_callback(directQuality[0].file, PROVIDER, PROVIDER, 'Hls', callback, 1, [], directQuality);
+                                return [2];
+                        }
+                    });
+                };
+                _i = 0, numes_1 = numes_2;
+                _a.label = 3;
             case 3:
-                embedData = _a.sent();
-                libs.log({ embedData: embedData, embedUrl: embedData.embed_url, key: embedData.key }, PROVIDER, "EMBED DATA");
-                if (!embedData.embed_url) {
-                    return [2];
-                }
-                decode = CryptoJSAesJson.decrypt(embedData.embed_url, embedData.key);
-                test = CryptoJSAesJson.decrypt('{"ct":"MoPfYyX4nzXH0JBT\\/W+BhTJ00a4BOwDm9OhU2ZTG282kFT8POtYvMPqHfLaLa8n24msyxmpzGx5I9M9un2q9MI4akw5iyAbtUnFwNAxesKc=","iv":"4154ab88e7ac123b529cc709ecd5e72a","s":"e67d038358bc2890","m":"wMywXOywHNzwHNyw3M0wHNxwHMzwHOzwXM8ZDfwwXMzwXMywHOywnNywXOxw3MzwHMxwHM0wHO8JTM8BjM8VTM8lDf4EDf2EDf1MDfyMDfzEDfxQDfyIDf1wnM8JDN8VjM8ZzM8RDfzw3Nzw3N8FTM8djM8lzM8dTM"}', '\\x57\\x69\\x79\\x49\\x30\\x59\\x52\\x4f\\x4d\\x4d\\x63\\x30\\x35\\x78\\x49\\x54\\x4d\\x4d\\x78\\x4d\\x55\\x78\\x54\\x3d\\x59\\x32\\x44\\x63\\x49\\x59\\x7a\\x4e\\x4d\\x30\\x54\\x57\\x59\\x57\\x4e\\x6a\\x54\\x4d\\x59\\x31');
-                libs.log({ decode: decode, test: test }, PROVIDER, 'decode');
-                if (!decode) {
-                    return [2];
-                }
-                return [4, libs.embed_redirect(decode, '', movieInfo, PROVIDER, callback, undefined, [])];
+                if (!(_i < numes_1.length)) return [3, 6];
+                nume = numes_1[_i];
+                return [5, _loop_1(nume)];
             case 4:
-                _a.sent();
-                return [3, 6];
+                state_1 = _a.sent();
+                if (typeof state_1 === "object")
+                    return [2, state_1.value];
+                _a.label = 5;
             case 5:
+                _i++;
+                return [3, 3];
+            case 6: return [3, 8];
+            case 7:
                 e_1 = _a.sent();
                 libs.log(e_1, PROVIDER, 'ERROR');
-                return [3, 6];
-            case 6: return [2, true];
+                return [3, 8];
+            case 8: return [2, true];
         }
     });
 }); };
