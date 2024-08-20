60cbf7341ec7760203a1548d12e92a7b729ee84a
haha <haha@gmail.com>
2024-08-21 00:30:21 +0700

    t

--- dist/hosts/vidplay.js
+++ dist/hosts/vidplay.js
@@ -36,6 +36,17 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
 };
 var _this = this;
 hosts["vid2faf"] = function (url, movieInfo, provider, config, callback) { return __awaiter(_this, void 0, void 0, function () {
+    function mapp(a, b, c) {
+        var d = b.length;
+        var e = {};
+        while (d-- && (e[b[d]] = c[d] || '')) {
+            ;
+        }
+        return a.split('').map(function (a) { return e[a] || a; }).join('');
+    }
+    function reverse(a) {
+        return a.split('').reverse().join('');
+    }
     function rc4(key, inp) {
         var arr = [];
         var counter = 0;
@@ -45,7 +56,6 @@ hosts["vid2faf"] = function (url, movieInfo, provider, config, callback) { retur
         for (i = 0; i < 256; i++) {
             arr[i] = i;
         }
-        libs.log({ key: key, inp: inp }, HOST, 'RC4');
         for (i = 0; i < 256; i++) {
             counter = (counter + arr[i] + key.charCodeAt(i % key.length)) % 256;
             tmp = arr[i];
@@ -64,54 +74,13 @@ hosts["vid2faf"] = function (url, movieInfo, provider, config, callback) { retur
         }
         return decrypted;
     }
-    function general_dec(key, inp) {
-        var i = libs.string_atob((inp).replace(/_/g, "/").replace(/-/g, "+"));
-        var e = rc4(key, i);
-        e = decodeURIComponent(e);
-        libs.log({ e: e }, HOST, 'general_dec_DECODED');
-        return e;
+    function subst(a) {
+        return (libs.string_btoa(a)).replace(/\//g, '_').replace(/\+/g, '-');
     }
-    function general_enc(key, inp) {
-        inp = encodeURIComponent(inp);
-        var e = rc4(key, inp);
-        var out = libs.string_btoa(e).replace(/\//g, "_").replace(/\+/g, '-');
-        return out;
+    function subst_(a) {
+        return libs.string_atob((a).replace(/_/g, '/').replace(/-/g, '+'));
     }
-    function _0x64802e(_0x5cf48e, _0x2c4055) {
-        var _0x25bb5e = [];
-        var _0x75e1c = 0;
-        var _0x42f028 = 0;
-        var _0x55312d;
-        var _0xf52ad3 = "";
-        for (_0x42f028 = 0; _0x42f028 < 256; _0x42f028++) {
-            _0x25bb5e[_0x42f028] = _0x42f028;
-        }
-        for (_0x42f028 = 0; _0x42f028 < 256; _0x42f028++) {
-            _0x75e1c = (_0x75e1c + _0x25bb5e[_0x42f028] + _0x5cf48e.charCodeAt(_0x42f028 % _0x5cf48e.length)) % 256;
-            _0x55312d = _0x25bb5e[_0x42f028];
-            _0x25bb5e[_0x42f028] = _0x25bb5e[_0x75e1c];
-            _0x25bb5e[_0x75e1c] = _0x55312d;
-        }
-        _0x42f028 = 0;
-        for (var _0x28ab3a = _0x75e1c = 0; _0x28ab3a < _0x2c4055.length; _0x28ab3a++) {
-            _0x42f028 = (_0x42f028 + 1) % 256;
-            _0x75e1c = (_0x75e1c + _0x25bb5e[_0x42f028]) % 256;
-            _0x55312d = _0x25bb5e[_0x42f028];
-            _0x25bb5e[_0x42f028] = _0x25bb5e[_0x75e1c];
-            _0x25bb5e[_0x75e1c] = _0x55312d;
-            _0xf52ad3 += String.fromCharCode(_0x2c4055.charCodeAt(_0x28ab3a) ^ _0x25bb5e[(_0x25bb5e[_0x42f028] + _0x25bb5e[_0x75e1c]) % 256]);
-        }
-        return _0xf52ad3;
-    }
-    function _0x32df35(_0x358076, _0x4796ca, _0x2f37d2) {
-        var _0x37d225 = _0x4796ca.length;
-        var _0x1d2dda = {};
-        while (_0x37d225-- && (_0x1d2dda[_0x4796ca[_0x37d225]] = _0x2f37d2[_0x37d225] || "")) {
-            ;
-        }
-        return _0x358076.split("").map(function (_0x39e617) { return _0x1d2dda[_0x39e617] || _0x39e617; }).join("");
-    }
-    var DOMAIN, HOST, PARSEDOMAIN, subParse, t, subs, headers, embed_enc, get_keys_1, h_enc, embed_dec, keys_1, embed_enc_2_1, h_enc_2_1, embed_dec_2_1, id_1, stream, playlist, rank, _i, _a, embedItem, embedData, patternQuality, directQuality, _b, patternQuality_1, patternItem, sizeQuality, urlDirect, urlDirect, e_1, e1_1;
+    var DOMAIN, HOST, PARSEDOMAIN, subParse, t, subs, headers, keys_1, enc_1, dec_1, id_1, stream, playlist, rank, _i, _a, embedItem, embedData, patternQuality, directQuality, _b, patternQuality_1, patternItem, sizeQuality, urlDirect, urlDirect, e_1, e1_1;
     var _this = this;
     return __generator(this, function (_c) {
         switch (_c.label) {
@@ -139,74 +108,41 @@ hosts["vid2faf"] = function (url, movieInfo, provider, config, callback) { retur
                     'Referer': url,
                     'user-agent': libs.request_getRandomUserAgent()
                 };
-                embed_enc = function (inp) { return __awaiter(_this, void 0, void 0, function () {
-                    var keys;
-                    return __generator(this, function (_a) {
-                        switch (_a.label) {
-                            case 0: return [4, get_keys_1(PARSEDOMAIN)];
-                            case 1:
-                                keys = _a.sent();
-                                return [2, general_enc(keys[0], inp)];
-                        }
-                    });
-                }); };
-                get_keys_1 = function (host) { return __awaiter(_this, void 0, void 0, function () {
-                    var keys;
-                    return __generator(this, function (_a) {
-                        switch (_a.label) {
-                            case 0: return [4, libs.request_get("https://raw.githubusercontent.com/giammirove/videogatherer/main/src/keys.json")];
-                            case 1:
-                                keys = _a.sent();
-                                libs.log({ a: keys[host] }, HOST, 'KEYS');
-                                return [2, keys[host] || ""];
-                        }
-                    });
-                }); };
-                h_enc = function (inp) { return __awaiter(_this, void 0, void 0, function () {
-                    var keys;
-                    return __generator(this, function (_a) {
-                        switch (_a.label) {
-                            case 0: return [4, get_keys_1(PARSEDOMAIN)];
-                            case 1:
-                                keys = _a.sent();
-                                return [2, general_enc(keys[1], inp)];
-                        }
-                    });
-                }); };
-                embed_dec = function (inp) { return __awaiter(_this, void 0, void 0, function () {
-                    var keys;
-                    return __generator(this, function (_a) {
-                        switch (_a.label) {
-                            case 0:
-                                libs.log({ inp: inp }, HOST, 'EMBED DECODED');
-                                return [4, get_keys_1(PARSEDOMAIN)];
-                            case 1:
-                                keys = _a.sent();
-                                return [2, general_dec(keys[2], inp)];
-                        }
-                    });
-                }); };
                 return [4, libs.request_get("https://raw.githubusercontent.com/giammirove/videogatherer/main/src/keys.json")];
             case 4:
                 keys_1 = _c.sent();
                 keys_1 = keys_1["vid2faf.site"] || [];
-                embed_enc_2_1 = function (embed_id) {
-                    var _0xa8a16f;
-                    _0xa8a16f = _0x32df35(_0xa8a16f = ("" + libs.string_btoa(_0x64802e(keys_1[0], _0xa8a16f = "" + (_0xa8a16f = embed_id)))).replace(/\//g, "_").replace(/\+/g, "-"), keys_1[1], keys_1[2]);
-                    _0xa8a16f = ("" + libs.string_btoa(_0x64802e(keys_1[5], _0xa8a16f = _0x32df35(_0xa8a16f = _0xa8a16f.split("").reverse().join(""), keys_1[3], keys_1[4])))).replace(/\//g, "_").replace(/\+/g, "-");
-                    _0xa8a16f = ("" + libs.string_btoa(_0x64802e(keys_1[6], _0xa8a16f = _0xa8a16f.split("").reverse().join("")))).replace(/\//g, "_").replace(/\+/g, "-");
-                    _0xa8a16f = ("" + libs.string_btoa(_0xa8a16f = (_0xa8a16f = _0x32df35(_0xa8a16f, keys_1[7], keys_1[8])).split("").reverse().join(""))).replace(/\//g, "_").replace(/\+/g, "-");
-                    return _0xa8a16f;
+                enc_1 = function (inp) {
+                    var a = mapp(inp, keys_1[0], keys_1[1]);
+                    a = reverse(a);
+                    a = rc4(keys_1[2], a);
+                    a = subst(a);
+                    a = reverse(a);
+                    a = mapp(a, keys_1[3], keys_1[4]);
+                    a = rc4(keys_1[5], a);
+                    a = subst(a);
+                    a = rc4(keys_1[6], a);
+                    a = subst(a);
+                    a = reverse(a);
+                    a = mapp(a, keys_1[7], keys_1[8]);
+                    a = subst(a);
+                    return a;
                 };
-                h_enc_2_1 = function (embed_id) {
-                    return libs.string_btoa(_0x64802e(keys_1[9], embed_id));
-                };
-                embed_dec_2_1 = function (data) {
-                    var _0x3510d5 = libs.string_atob(("" + (_0x3510d5 = "" + (_0x3510d5 = data))).replace(/_/g, "/").replace(/-/g, "+"));
-                    _0x3510d5 = _0x64802e(keys_1[6], libs.string_atob(("" + (_0x3510d5 = _0x32df35(_0x3510d5 = _0x3510d5.split("").reverse().join(""), keys_1[8], keys_1[7]))).replace(/_/g, "/").replace(/-/g, "+")));
-                    _0x3510d5 = _0x32df35(_0x3510d5 = _0x64802e(keys_1[5], libs.string_atob(("" + (_0x3510d5 = _0x3510d5.split("").reverse().join(""))).replace(/_/g, "/").replace(/-/g, "+"))), keys_1[4], keys_1[3]);
-                    _0x3510d5 = _0x64802e(keys_1[0], libs.string_atob(("" + (_0x3510d5 = _0x32df35(_0x3510d5 = _0x3510d5.split("").reverse().join(""), keys_1[2], keys_1[1]))).replace(/_/g, "/").replace(/-/g, "+")));
-                    return _0x3510d5;
+                dec_1 = function (inp) {
+                    var a = subst_(inp);
+                    a = mapp(a, keys_1[8], keys_1[7]);
+                    a = reverse(a);
+                    a = subst_(a);
+                    a = rc4(keys_1[6], a);
+                    a = subst_(a);
+                    a = rc4(keys_1[5], a);
+                    a = mapp(a, keys_1[4], keys_1[3]);
+                    a = reverse(a);
+                    a = subst_(a);
+                    a = rc4(keys_1[2], a);
+                    a = reverse(a);
+                    a = mapp(a, keys_1[1], keys_1[0]);
+                    return a;
                 };
                 id_1 = url.match(/\/e\/([^\?]+)/i);
                 id_1 = id_1 ? id_1[1] : '';
@@ -214,21 +150,20 @@ hosts["vid2faf"] = function (url, movieInfo, provider, config, callback) { retur
                     return [2];
                 }
                 stream = function (url) { return __awaiter(_this, void 0, void 0, function () {
-                    var search, embed_id, h, hash, mediainfo_url, resp, playlist;
+                    var search, embed_id, hash, mediainfo_url, resp, playlist;
                     return __generator(this, function (_a) {
                         switch (_a.label) {
                             case 0:
                                 search = url.replace("".concat(DOMAIN, "/e/").concat(id_1), "");
                                 embed_id = id_1;
-                                h = h_enc_2_1(embed_id);
-                                hash = embed_enc_2_1(embed_id);
-                                mediainfo_url = "".concat(DOMAIN, "/mediainfo/").concat(hash).concat(search, "&ads=0&h=").concat(encodeURIComponent(h));
+                                hash = enc_1(embed_id);
+                                mediainfo_url = "".concat(DOMAIN, "/mediainfo/").concat(hash).concat(search, "&ads=0");
                                 return [4, fetch(mediainfo_url)];
                             case 1: return [4, (_a.sent()).json()];
                             case 2:
                                 resp = _a.sent();
                                 libs.log({ mediainfo_url: mediainfo_url, hash: hash, resp: resp }, HOST, 'MEDIAINFO URL');
-                                playlist = (embed_dec_2_1(resp['result'])).replace(/\\\//g, "/");
+                                playlist = (dec_1(resp['result'])).replace(/\\\//g, "/");
                                 return [2, JSON.parse(playlist)];
                         }
                     });
