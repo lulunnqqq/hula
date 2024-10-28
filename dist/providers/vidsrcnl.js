922d778e0ea1011fc7514662b574257ae83d14ba
haha <haha@gmail.com>
2024-10-28 22:10:01 +0700

    t

--- dist/providers/vidsrcnl.js
+++ dist/providers/vidsrcnl.js
@@ -36,7 +36,7 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
 };
 var _this = this;
 source.getResource = function (movieInfo, config, callback) { return __awaiter(_this, void 0, void 0, function () {
-    var PROVIDER, DOMAIN, headers, urlSearch, parseSearch, iframeURL, parseIframe, directURL, e_1;
+    var PROVIDER, DOMAIN, headers, urlSearch, parseSearch, iframeURL, parseIframeURL_1, urls_2, _loop_1, _i, urls_1, item, state_1, e_1;
     return __generator(this, function (_a) {
         switch (_a.label) {
             case 0:
@@ -49,7 +49,7 @@ source.getResource = function (movieInfo, config, callback) { return __awaiter(_
                 };
                 _a.label = 1;
             case 1:
-                _a.trys.push([1, 4, , 5]);
+                _a.trys.push([1, 8, , 9]);
                 urlSearch = "https://player.vidsrc.nl/embed/movie/".concat(movieInfo.tmdb_id);
                 if (movieInfo.type == 'tv') {
                     urlSearch = "https://player.vidsrc.nl/embed/tv/".concat(movieInfo.tmdb_id, "/").concat(movieInfo.season, "/").concat(movieInfo.episode);
@@ -62,23 +62,82 @@ source.getResource = function (movieInfo, config, callback) { return __awaiter(_
                 if (!iframeURL) {
                     return [2];
                 }
-                iframeURL = iframeURL.replace("/embed/", "/stream/");
-                libs.log({ iframeURL: iframeURL }, PROVIDER, "IFRAME PARSE");
+                if (!(iframeURL.indexOf("hyplexnetworks") != -1)) return [3, 7];
                 return [4, libs.request_get(iframeURL, headers, true)];
             case 3:
-                parseIframe = _a.sent();
-                directURL = parseIframe('media-player').attr("src");
-                libs.log({ directURL: directURL }, PROVIDER, "DIRECT URL");
-                if (!directURL) {
+                parseIframeURL_1 = _a.sent();
+                urls_2 = [];
+                parseIframeURL_1("#dropdownMenu a").each(function (key, item) {
+                    var u = parseIframeURL_1(item).attr('data-url');
+                    if (u) {
+                        var e = u.replace("https://node.hyplexnetworks.pw/embed/decode.php?url=", "");
+                        e = libs.string_base64_decode(e);
+                        urls_2.push(e);
+                    }
+                });
+                libs.log({ urls: urls_2 }, PROVIDER, "PFIRAME");
+                if (!urls_2.length) {
                     return [2];
                 }
-                libs.embed_callback(directURL, PROVIDER, PROVIDER, 'Hls', callback, 1, [], [{ file: directURL, quality: 1080 }]);
-                return [3, 5];
+                _loop_1 = function (item) {
+                    var parseDirect, script, unpacker, file;
+                    return __generator(this, function (_b) {
+                        switch (_b.label) {
+                            case 0:
+                                if (item.indexOf("vidhidepro") == -1) {
+                                    return [2, "continue"];
+                                }
+                                return [4, libs.request_get(item, {
+                                        "Sec-Fetch-Site": "same-origin",
+                                        "Sec-Fetch-Mode": "navigate",
+                                        "Sec-Fetch-Dest": "iframe"
+                                    }, true)];
+                            case 1:
+                                parseDirect = _b.sent();
+                                script = "";
+                                parseDirect("script").each(function (key, item) {
+                                    var text = parseDirect(item).text();
+                                    if (text.indexOf("eval(") != -1) {
+                                        script = text;
+                                    }
+                                });
+                                libs.log({ script: script }, PROVIDER, 'SCRIPT');
+                                if (!script) {
+                                    return [2, { value: void 0 }];
+                                }
+                                unpacker = libs.string_unpacker_v2(script);
+                                libs.log({ unpacker: unpacker }, PROVIDER, 'Unpacker');
+                                file = unpacker.match(/file *\: *\"([^\"]+)/i);
+                                file = file ? file[1] : '';
+                                libs.log({ file: file }, PROVIDER, 'FILE');
+                                if (!file) {
+                                    return [2, { value: void 0 }];
+                                }
+                                libs.embed_callback(file, PROVIDER, PROVIDER, 'Hls', callback, 1, [], [{ file: file, quality: 1080 }]);
+                                return [2];
+                        }
+                    });
+                };
+                _i = 0, urls_1 = urls_2;
+                _a.label = 4;
             case 4:
+                if (!(_i < urls_1.length)) return [3, 7];
+                item = urls_1[_i];
+                return [5, _loop_1(item)];
+            case 5:
+                state_1 = _a.sent();
+                if (typeof state_1 === "object")
+                    return [2, state_1.value];
+                _a.label = 6;
+            case 6:
+                _i++;
+                return [3, 4];
+            case 7: return [3, 9];
+            case 8:
                 e_1 = _a.sent();
                 libs.log({ e: e_1 }, PROVIDER, "ERROR");
-                return [3, 5];
-            case 5: return [2];
+                return [3, 9];
+            case 9: return [2];
         }
     });
 }); };
