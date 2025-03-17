ea6c50f7f2549edd0a8bda08d2179198f79bcbdb
haha <haha@gmail.com>
2025-03-17 21:38:54 +0700

    t

--- dist/providers/vidlink.js
+++ dist/providers/vidlink.js
@@ -1,3 +1,14 @@
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -65,7 +76,7 @@ source.getResource = function (movieInfo, config, callback) { return __awaiter(_
         }
         return result;
     }
-    var PROVIDER, DOMAIN, headers, headerDirect, decryptjs, urlEmbed, parseEmbed_1, scripts_2, keyEndpoint, _i, scripts_1, item, scriptData, textData, matchEndpoint, hash, textHash, urlSearch, parseSearch, directUrl, tracks, _a, _b, item, type, directQuality_1, qualityIndex, q, qualityItem, parseDirecturl, decodeHeader, parseDirectSize, textDirect, m3u8Data, directQuality_2, directSizes, patternSize, directQuality, _c, patternSize_1, patternItem, sizeQuality, e_1;
+    var PROVIDER, DOMAIN, headers, headerDirect, decryptjs, urlEmbed, parseEmbed_1, scripts_2, keyEndpoint, _i, scripts_1, item, scriptData, textData, matchEndpoint, hash, textHash, urlSearch, parseSearch, directUrl, tracks, _a, _b, item, type, directQuality_1, qualityIndex, q, qualityItem, proxyDirectUrl, parseDirecturl, parseDirectSize, textDirect, m3u8Data, directQuality_2, directSizes, patternSize, directQuality, _c, patternSize_1, patternItem, sizeQuality, e_1;
     return __generator(this, function (_d) {
         switch (_d.label) {
             case 0:
@@ -222,12 +233,16 @@ source.getResource = function (movieInfo, config, callback) { return __awaiter(_
                         return [2];
                     }
                 }
+                proxyDirectUrl = directUrl.split("/proxy/");
+                if (proxyDirectUrl.length > 1) {
+                    directUrl = decodeURIComponent(proxyDirectUrl[1]);
+                }
                 parseDirecturl = directUrl.split("m3u8/{");
                 if (parseDirecturl.length > 1) {
                     directUrl = parseDirecturl[0] + "m3u8";
-                    decodeHeader = encodeURIComponent("{" + parseDirecturl[1]);
-                    directUrl = directUrl + "/" + decodeHeader;
+                    headerDirect = __assign(__assign({}, JSON.parse("{" + parseDirecturl[1])), { "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36" });
                 }
+                libs.log({ directUrl: directUrl, headerDirect: headerDirect }, PROVIDER, 'DIRECT URL');
                 return [4, fetch(directUrl)];
             case 11:
                 parseDirectSize = _d.sent();
