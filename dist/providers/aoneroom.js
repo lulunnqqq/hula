80f000eef9c3467aeb89c69ef53a3cc13ee05466
haha <haha@gmail.com>
2025-04-03 09:05:45 +0700

    h

--- dist/providers/aoneroom.js
+++ dist/providers/aoneroom.js
@@ -40,7 +40,7 @@ source.getResource = function (movieInfo, config, callback) { return __awaiter(_
     return __generator(this, function (_f) {
         switch (_f.label) {
             case 0:
-                PROVIDER = 'SAoneroom';
+                PROVIDER = 'CAoneroom';
                 DOMAIN = "https://h5.aoneroom.com";
                 headers = {
                     'user-agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36",
