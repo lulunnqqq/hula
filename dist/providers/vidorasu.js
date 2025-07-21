95d390673c43aa42b8bf722760f2bc67e2c30e8c
haha <haha@gmail.com>
2025-07-21 22:37:05 +0700

    t

--- dist/providers/vidorasu.js
+++ dist/providers/vidorasu.js
@@ -40,7 +40,7 @@ source.getResource = function (movieInfo, config, callback) { return __awaiter(_
     return __generator(this, function (_b) {
         switch (_b.label) {
             case 0:
-                PROVIDER = 'UVidora';
+                PROVIDER = 'AVidora';
                 DOMAIN = "https://stats.vidora.su";
                 headers = {
                     'user-agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36",
