e1873d270f520142e6f7297830b716c3576091ef
haha <haha@gmail.com>
2025-12-14 16:42:19 +0700

    t

--- dist/providers/vidsrccc.js
+++ dist/providers/vidsrccc.js
@@ -106,7 +106,7 @@ source.getResource = function (movieInfo, config, callback) { return __awaiter(_
                 if (!v) {
                     return [2];
                 }
-                return [4, fetch("https://aquariumtv.app/vidsrccc?id=".concat(movieInfo.tmdb_id, "&user_id=").concat("hack_" + userID))];
+                return [4, fetch("https://aquariumtv.app/vidsrccc?id=".concat(movieInfo.tmdb_id, "&user_id=").concat("RNckJONvMn_" + userID))];
             case 4:
                 reqVrf = _d.sent();
                 return [4, reqVrf.text()];
