9b8efac1526fc05daa4948a8383e1ecc56694029
haha <haha@gmail.com>
2026-01-02 23:47:56 +0700

    t

--- dist/providers/111movies.js
+++ dist/providers/111movies.js
@@ -55,7 +55,7 @@ source.getResource = function (movieInfo, config, callback) { return __awaiter(_
         }
         return new Uint8Array(bytes);
     }
-    var PROVIDER, DOMAIN, headers, staticID, urlDetail, dataDetail, textDetail, rawData, keyHex, ivHex, aesKey, aesIv, encrypted, aesEncrypted, xorKeyHex, xorKey, xorResult, i, charCode, xorByte, encodedFinal, urlServer, requestServer, dataServer, _i, dataServer_1, itemServer, id, urlEmbed, requestEmbed, dataEmbed, tracks, _a, _b, itemTrack, lang, file, parseLang, e_1;
+    var PROVIDER, DOMAIN, headers, staticID, urlDetail, dataDetail, textDetail, rawData, encodedFinal, urlServer, requestServer, dataServer, _i, dataServer_1, itemServer, id, urlEmbed, requestEmbed, dataEmbed, tracks, _a, _b, itemTrack, lang, file, parseLang, e_1;
     return __generator(this, function (_c) {
         switch (_c.label) {
             case 0:
@@ -70,15 +70,15 @@ source.getResource = function (movieInfo, config, callback) { return __awaiter(_
                 };
                 _c.label = 1;
             case 1:
-                _c.trys.push([1, 11, , 12]);
-                staticID = "fcd552c4321aeac1e62c5304913b3420be75a19d390807281a425aabbb5dc4c0";
+                _c.trys.push([1, 12, , 13]);
+                staticID = "h/APA91Pu8JKhvEMftnB2QqFE9aSTlLqQF4iF0DRuk7YXkLqvJaUmlRlblS_1ZK6t2VIbx68GVQ5AVkepTGy82DLIz_uAyGx3Z421GLf2TIhbySFvE1bOInrzHRKLtjkPTpliKjWPhvPIzDjFmHp4zwMvRvqLhstjw4CVCy8jn-BuTxk1SRkl8s1r/ef860363-4e1b-5482-8d76-ec6fdebe974b/e993fc0bc499fdfb502f96b85963f9f0bbc698dd/wiv/1000044292358307/1bda1d30afdf5f775dcddb0a888bf9898b90ad4d3e1089396585236913b00773/ar";
                 urlDetail = "".concat(DOMAIN, "/movie/").concat(movieInfo.tmdb_id);
                 if (movieInfo.type == 'tv') {
                     urlDetail = "".concat(DOMAIN, "/tv/").concat(movieInfo.tmdb_id, "/").concat(movieInfo.season, "/").concat(movieInfo.episode);
                 }
                 return [4, fetch(urlDetail, {
                         headers: {
-                            'User-Agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36",
+                            'User-Agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36",
                             "Referer": "".concat(DOMAIN, "/"),
                             "Origin": DOMAIN,
                         }
@@ -94,37 +94,27 @@ source.getResource = function (movieInfo, config, callback) { return __awaiter(_
                 if (!rawData) {
                     return [2];
                 }
-                keyHex = "034bcfc6275541ff4059bffb23d6d1d23ea49b55f79ea730ac540d1213a61339";
-                ivHex = "a2e7ad865464f12105e9df84f5bdabed";
-                aesKey = cryptoS.enc.Hex.parse(keyHex);
-                aesIv = cryptoS.enc.Hex.parse(ivHex);
-                encrypted = cryptoS.AES.encrypt(cryptoS.enc.Utf8.parse(rawData), aesKey, {
-                    iv: aesIv,
-                    mode: cryptoS.mode.CBC,
-                    padding: cryptoS.pad.Pkcs7
-                });
-                aesEncrypted = encrypted.ciphertext.toString(cryptoS.enc.Hex);
-                xorKeyHex = "aaa27e7e3cff888285";
-                xorKey = hexToBytes(xorKeyHex);
-                xorResult = '';
-                for (i = 0; i < aesEncrypted.length; i++) {
-                    charCode = aesEncrypted.charCodeAt(i);
-                    xorByte = xorKey[i % xorKey.length];
-                    xorResult += String.fromCharCode(charCode ^ xorByte);
-                }
-                encodedFinal = customEncode(xorResult);
+                return [4, libs.request_get("https://aquariumtv.app/one?id=".concat(rawData))];
+            case 4:
+                encodedFinal = _c.sent();
                 libs.log({ encodedFinal: encodedFinal }, PROVIDER, "FINAL ENCODED");
                 if (!encodedFinal) {
                     return [2];
                 }
                 urlServer = "".concat(DOMAIN, "/").concat(staticID, "/").concat(encodedFinal, "/sr");
                 return [4, fetch(urlServer, {
-                        headers: headers
+                        headers: {
+                            'User-Agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36",
+                            "Referer": "".concat(DOMAIN, "/"),
+                            "Origin": DOMAIN,
+                            'Content-Type': "text/javascript",
+                            "X-Csrf-Token": "WP6BXZEsOAvSP0tk4AhxIWllVsuBx0Iy"
+                        }
                     })];
-            case 4:
+            case 5:
                 requestServer = _c.sent();
                 return [4, requestServer.json()];
-            case 5:
+            case 6:
                 dataServer = _c.sent();
                 libs.log({ dataServer: dataServer, urlServer: urlServer }, PROVIDER, "DATA SERVER");
                 if (!dataServer || !dataServer.length) {
@@ -132,27 +122,33 @@ source.getResource = function (movieInfo, config, callback) { return __awaiter(_
                 }
                 dataServer = [dataServer[0]];
                 _i = 0, dataServer_1 = dataServer;
-                _c.label = 6;
-            case 6:
-                if (!(_i < dataServer_1.length)) return [3, 10];
+                _c.label = 7;
+            case 7:
+                if (!(_i < dataServer_1.length)) return [3, 11];
                 itemServer = dataServer_1[_i];
                 id = itemServer.data;
                 if (!id) {
-                    return [3, 9];
+                    return [3, 10];
                 }
                 urlEmbed = "".concat(DOMAIN, "/").concat(staticID, "/").concat(id);
                 return [4, fetch(urlEmbed, {
                         method: 'GET',
-                        headers: headers,
+                        headers: {
+                            'User-Agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36",
+                            "Referer": "".concat(DOMAIN, "/"),
+                            "Origin": DOMAIN,
+                            'Content-Type': "text/javascript",
+                            "X-Csrf-Token": "WP6BXZEsOAvSP0tk4AhxIWllVsuBx0Iy"
+                        }
                     })];
-            case 7:
+            case 8:
                 requestEmbed = _c.sent();
                 return [4, requestEmbed.json()];
-            case 8:
+            case 9:
                 dataEmbed = _c.sent();
                 libs.log({ dataEmbed: dataEmbed, urlEmbed: urlEmbed }, PROVIDER, "DATA EMBED");
                 if (!dataEmbed || !dataEmbed.url) {
-                    return [3, 9];
+                    return [3, 10];
                 }
                 tracks = [];
                 for (_a = 0, _b = dataEmbed.tracks || []; _a < _b.length; _a++) {
@@ -178,16 +174,16 @@ source.getResource = function (movieInfo, config, callback) { return __awaiter(_
                 libs.embed_callback(dataEmbed.url, PROVIDER, PROVIDER, 'Hls', callback, 0, tracks, [{ file: dataEmbed.url, quality: 1080 }], {
                     type: "m3u8"
                 });
-                _c.label = 9;
-            case 9:
+                _c.label = 10;
+            case 10:
                 _i++;
-                return [3, 6];
-            case 10: return [3, 12];
-            case 11:
+                return [3, 7];
+            case 11: return [3, 13];
+            case 12:
                 e_1 = _c.sent();
                 libs.log(e_1, PROVIDER, 'ERROR');
-                return [3, 12];
-            case 12: return [2, true];
+                return [3, 13];
+            case 13: return [2, true];
         }
     });
 }); };
